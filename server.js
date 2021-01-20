dc8165f15aceb16bc00ea4e30bf733620a140515
Miranda McDonald <mirandagrace.dev@gmail.com>
2021-01-19 21:59:27 -0500

    created routs for index.html and notes.html

--- server.js
+++ server.js
@@ -1,25 +1,30 @@
-// Dependencies
+// dependencies
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
 
-// Define a PORT to listen on
+// create a port for the app to run
 const PORT = process.env.PORT || 3000;
 
-// Create a server using Express
+// create an instance of express
 const app = express();
 
-// Set up the Express app to handle data parsing
+// handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+//return the index.html file
 app.get("/", function (req, res) {
-	res.json(path.join(__dirname, "public/index.html"));
+	res.json(path.join(__dirname, "/public/index.html"));
 });
 
+//return the notes.html file
+app.get("/notes", (req, res) => {
+	res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+// start the server
 app.listen(PORT, () => {
-	console.log("hooray");
+	console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-
